0a17b4b000f426becb6edffc1974333e2acc0e50
OstrichWhite <rahimcsestudies@gmail.com>
2022-09-21 18:49:23 +0530

    Refactored studentController,app.

--- app.js
+++ app.js
@@ -2,15 +2,12 @@ import express from 'express';
 import dotenv from 'dotenv';
 import {createStudent,getStudent,getStudents,deleteStudent,updateStudent,ping} from './controllers/studentController'
 
-// const express = require('express');
-// const dotenv = require('dotenv');
-// const studentController = require('./controllers/studentController');
-
 dotenv.config({ path: './config.env' });
 const app=express()
 
 app.use(express.json());
 
+app.get('/ping',ping)
 app.get('/students',getStudents)
 app.post('/students',createStudent);
 app
--- controllers/studentController.js
+++ controllers/studentController.js
@@ -1,11 +1,11 @@
 import { Low, JSONFile } from 'lowdb'
 
 const adapter = new JSONFile('./db.json');
-const db = new Low(adapter)
+const db = new Low(adapter);
 
-const createStudent= async (req,res)=>{
-  const {name,rollno,contacts,subjects} = req.body;
-  const student =  {
+const createStudent= async (req, res) => {
+  const { name, rollno, contacts, subjects} = req.body;
+  const student = {
     id:Date.now(),
     name,
     rollno,
@@ -18,54 +18,59 @@ const createStudent= async (req,res)=>{
 
   res.status(201).json({
     status:'success',
-    message:'Student created successfully' ,
-    data:student,
+    message:'Student created successfully',
+    student,
   });
-}
+};
 
-const getStudent = async (req,res)=>{
+const getStudent = async (req, res) =>{
   const studentId =Number(req.params.id);
   await db.read();
   const student=db.data.students.find(student=>student.id===studentId);
 
-  res.status(200).json({status:'success',data:student});
-}
+  res.status(200).json({ status:'success', student });
+};
 
 const getStudents = async (req,res)=>{
   await db.read();
-
-  res.status(200).json({status:'success',data:db.data.students});
-}
+  const students =db.data.students;
+  res.status(200).json({
+    status:'success', 
+    results:students.length , 
+    students,
+  });
+};
 
 const deleteStudent = async (req,res)=>{
   const studentId = Number(req.params.id);
   await db.read();
   const students = db.data.students;
-  const indexOfStudent = students.findIndex(student=>student.id===studentId)
+  const indexOfStudent = students.findIndex(student=>student.id===studentId);
   students.splice(indexOfStudent,1);
-  await db.write()
+  await db.write();
 
-  res.status(204).json({status:'success',message:'Student deleted successfully'})
-}
+  res.status(204).json({ status:'success', message:'Student deleted successfully' })
+};
 
 const updateStudent = async (req,res)=>{
-  const updatedStudent = req.body;
+  const {name,rollno,contacts,subjects} = req.body;
+  const updatedStudent = {name,rollno,contacts,subjects};
   const studentId = Number(req.params.id);
   await db.read();
   const students = db.data.students;
   const indexOfStudent = students.findIndex(student=>student.id===studentId);
   students[indexOfStudent]= { id:studentId, ...updatedStudent };
-  await db.write()
+  await db.write();
 
   res.status(200).json({
     status:'success',
     message:'Student updated successfully',
-    data:updatedStudent,
-  })
-}
+    student:updatedStudent,
+  });
+};
 
 const ping = (req,res)=>{
-  res.send('pong')
-}
+  res.send('pong');
+};
 
-export {createStudent,getStudent,getStudents,deleteStudent,updateStudent,ping}
+export {createStudent,getStudent,getStudents,deleteStudent,updateStudent,ping};
