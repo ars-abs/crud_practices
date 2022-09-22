40568472c9e40142962885949d8e120d198d1a80
OstrichWhite <rahimcsestudies@gmail.com>
2022-09-22 17:24:09 +0530

    Introduced factoryResource.

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import student from './resources/student';
+import factoryResource from './resources/factoryResource';
 import ping from './controllers/ping';
 import registerResource from './lib/registerResource';
 
@@ -10,8 +10,8 @@ const app = express();
 app.use(express.json());
 
 app.get('/ping', ping);
-registerResource({ app, path: 'students', resource: student });
-registerResource({ app, path: 'teachers', resource: student });
+registerResource({ app, path: 'students', resource: factoryResource });
+registerResource({ app, path: 'teachers', resource: factoryResource });
 
 
 const port = process.env.PORT;
