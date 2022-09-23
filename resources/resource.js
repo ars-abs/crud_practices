1afa1febc52704bb071e1194c663bc333279d759
OstrichWhite <rahimcsestudies@gmail.com>
2022-09-23 14:37:12 +0530

    resource refactored.

--- resources/resource.js
+++ resources/resource.js
@@ -7,63 +7,61 @@ const resource = ({ app, path, allowedFields }) => {
   const filterObj = (obj, allowedFields) => {
     const newObj = {};
     Object.keys(obj).map((fields) => {
-      if(allowedFields.includes(fields)) newObj[fields] = obj[fields];
+      if (allowedFields.includes(fields)) newObj[fields] = obj[fields];
     });
     return newObj;
   };
 
-  const filterBody = (req,res,next,allowedFields)=>{
-    req.body= filterObj(req.body,allowedFields);
+  const filterBody = (req, res, next, allowedFields) => {
+    req.body = filterObj(req.body, allowedFields);
     next();
   }
 
-  const resources = {
-    create: async (req, res) => {
-      await repo.create(req.body);
+  const create = async (req, res) => {
+    await repo.create(req.body);
 
-      res.status(201).json({
-        status: 'success',
-        data: req.body,
-      });
-    },
-
-    get: async (req, res) => {
-      const data = await repo.get(req.params.id)
-      res.status(200).json({ status: 'success', data });
-    },
-
-    getAll: async (req, res) => {
-      const data = await repo.getAll();
-      res.status(200).json({
-        status: 'success',
-        results: data.length,
-        data,
-      });
-    },
-
-    remove: async (req, res) => {
-      await repo.remove(req.params.id);
-
-      res.status(204).json({ status: 'success', message: 'Deleted successfully.' })
-    },
-
-    update: async (req, res) => {
-      const data=req.body;
-      await repo.update(req.params.id,data)
-
-      res.status(200).json({
-        status: 'success',
-        data,
-      });
-    },
+    res.status(201).json({
+      status: 'success',
+      data: req.body,
+    });
   };
 
-  app.get(`/${path}`, resources.getAll);
-  app.post(`/${path}`, (req,res,next)=>filterBody(req,res,next,allowedFields) ,resources.create);
+  const get = async (req, res) => {
+    const data = await repo.get(req.params.id)
+    res.status(200).json({ status: 'success', data });
+  };
+
+  const getAll = async (req, res) => {
+    const data = await repo.getAll();
+    res.status(200).json({
+      status: 'success',
+      results: data.length,
+      data,
+    });
+  };
+
+  const remove = async (req, res) => {
+    await repo.remove(req.params.id);
+
+    res.status(204).json({ status: 'success', message: 'Deleted successfully.' });
+  };
+
+  const update = async (req, res) => {
+    const data = req.body;
+    await repo.update(req.params.id, data)
+
+    res.status(200).json({
+      status: 'success',
+      data,
+    });
+  };
+
+  app.get(`/${path}`, getAll);
+  app.post(`/${path}`, (req, res, next) => filterBody(req, res, next, allowedFields), create);
   app
-    .get(`/${path}/:id`, resources.get)
-    .put(`/${path}/:id`, (req,res,next)=>filterBody(req,res,next,allowedFields) ,resources.update)
-    .delete(`/${path}/:id`, resources.remove);
+    .get(`/${path}/:id`, get)
+    .put(`/${path}/:id`, (req, res, next) => filterBody(req, res, next, allowedFields), update)
+    .delete(`/${path}/:id`, remove);
 };
 
 export default resource;
