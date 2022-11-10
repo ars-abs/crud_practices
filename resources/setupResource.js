98c56fe5d611826f9077b0df428b3794fc714045
abdul-abstractit <ars@abstractit.in>
2022-11-10 19:49:28 +0530

    Refactored.

--- resources/setupResource.js
+++ resources/setupResource.js
@@ -1,25 +1,27 @@
-import { map } from "@laufire/utils/collection";
-import resource from "./resource";
-import translateSchema from "./translateSchema";
-import standardizeResources from "./standardizeResources";
+import { map } from '@laufire/utils/collection';
+import resource from './resource';
+import translateSchema from './translateSchema';
+import standardizeResources from './standardizeResources';
 
 const setupResource = (context) => {
-  const {app, resources} = context
-  const standardizedResources = standardizeResources(resources);
-  map(standardizedResources, (standardizedResource) => {
-    const { 
-      name, 
-      schema,
-      repo: { type: repoType, path: repoPath }, 
-    } = standardizedResource;
-    resource({
-      app,
-      name,
-      schema: translateSchema({ repoType, schema }),
-      repoType,
-      repoPath,
-    })
-  })
-}
+	const { app, resources } = context;
+	const standardizedResources = standardizeResources(resources);
+
+	map(standardizedResources, (standardizedResource) => {
+		const {
+			name,
+			schema,
+			repo: { type: repoType, path: repoPath },
+		} = standardizedResource;
+
+		resource({
+			app: app,
+			name: name,
+			schema: translateSchema({ repoType, schema }),
+			repoType: repoType,
+			repoPath: repoPath,
+		});
+	});
+};
 
 export default setupResource;
