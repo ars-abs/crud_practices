f24eaf2bfe444e5fcecd12bb4633181cd50c95e2
abdul-abstractit <ars@abstractit.in>
2023-03-14 16:20:58 +0530

    Test refactored.

--- setupMiddleWares.test.js
+++ setupMiddleWares.test.js
@@ -3,20 +3,21 @@ import cors from 'cors';
 import express from 'express';
 import setupMiddleWares from './setupMiddleWares';
 
-jest.mock('cors', () => jest.fn(() => 'hello'));
+jest.mock('cors');
 
 test('setupMiddleware', () => {
 	const app = { use: jest.fn() };
 	const jsonRtnVal = Symbol('json');
+	const corsRtnVal = Symbol('cors');
 
 	jest.spyOn(express, 'json').mockReturnValue(jsonRtnVal);
 	jest.spyOn(apiLog, 'default').mockReturnValue();
+	cors.mockImplementation(() => corsRtnVal);
 
 	setupMiddleWares({ app });
 
 	expect(cors).toHaveBeenCalledWith({ origin: '*' });
-
-	expect(app.use).toHaveBeenCalledWith('hello');
+	expect(app.use).toHaveBeenCalledWith(corsRtnVal);
 	expect(app.use).toHaveBeenCalledWith(apiLog.default);
 	expect(app.use).toHaveBeenCalledWith(jsonRtnVal);
 });
