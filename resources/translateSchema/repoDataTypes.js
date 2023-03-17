ffa96464db8eebd10acb732a709b31f99db9d0f4
abdul-abstractit <ars@abstractit.in>
2023-03-17 15:28:10 +0530

    Data types added.

--- resources/translateSchema/repoDataTypes.js
+++ resources/translateSchema/repoDataTypes.js
@@ -6,12 +6,16 @@ const repoDataTypes = {
 		boolean: Boolean,
 		number: Number,
 		object: Object,
+		date: Date,
+		float: Float32Array,
 	},
 	sequelize: {
 		string: DataTypes.STRING,
 		boolean: DataTypes.BOOLEAN,
 		number: DataTypes.INTEGER,
 		object: DataTypes.JSONB,
+		date: DataTypes.DATE,
+		float: DataTypes.FLOAT,
 	},
 };
 
